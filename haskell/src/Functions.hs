3ba7eba93e3c071752c4693799d2eceef5fc977a
Kauã Santana <kauassilva0023@gmail.com>
2026-06-21 00:01:08 -0300

    troca o operador de potência para **, definido para Double.

--- haskell/src/Functions.hs
+++ haskell/src/Functions.hs
@@ -4,4 +4,4 @@ mean :: [Double] -> Double
 mean xs = sum xs / fromIntegral (length xs)
 
 stddev :: [Double] -> Double
-stddev xs = sqrt $ (sum xs - mean xs) ^ 2 / fromIntegral (length xs - 1)
+stddev xs = sqrt $ (sum xs - mean xs) ** 2 / fromIntegral (length xs - 1)
