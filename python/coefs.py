e8c67f043d63cc770cd6e255a7ae4f1754dc6ce1
Kauã Santana <kauassilva0023@gmail.com>
2026-09-24 00:39:28 -0300

    correção em algumas vaariaveis internas de b0 e no retorno de b1.

--- python/coefs.py
+++ python/coefs.py
@@ -4,8 +4,11 @@ def b1(x: list[float], y: list[float]) -> float:
     xs = sum(x)
     ys = sum(y)
     x2 = sum(xi**2 for xi in x)
-    return ((n * xy - xs * ys) / ((n * x2) - sum(x)*2)), n, ys, xs
+    return ((n * xy - xs * ys) / ((n * x2) - sum(x)**2))
 
 def b0(x: list[float], y: list[float]) -> float:
-    fb1, n, ys, xs = b1(x,y)
+    ys  = sum(y)
+    xs  = sum(x)
+    n   = len(x)
+    fb1 = b1(x,y)
     return (ys - fb1 * xs) / n
